3d14dce12bff0189b2d4f0141c876034e0261097
Ahmed Gamal <gis.csharp@gmail.com>
2021-03-25 01:28:09 +0300

    inital app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { ProductsComponent } from './products/products.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'orders', component: MyOrdersComponent },
+  { path: 'admin/orders', component: AdminOrdersComponent },
+  { path: 'admin/products', component: ProductsComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
