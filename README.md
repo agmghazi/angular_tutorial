c708c138455f67d2e25ea19b07028573055a0cac
Ahmed Gamal <agmghazi@hotmail.com>
2021-03-25 15:04:52 +0300

    fix issue

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ShoppingCartApp
+# GIS_Map
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.5.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ShoppingCartApp'`, () => {
+  it(`should have as title 'GIS_Map'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ShoppingCartApp');
+    expect(app.title).toEqual('GIS_Map');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ShoppingCartApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'GIS_Map app is running!'
+    );
   });
 });
