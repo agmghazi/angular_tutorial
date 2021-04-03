15359a6b6f5edd25068345592f6a5b7e7faa48a1
Ahmed Gamal <gis.csharp@gmail.com>
2021-04-03 15:19:47 +0300

    Auto-generated commit

--- polyfills.js
+++ polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Sourse\angular 6\code\angular_tutorial\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! D:\NEW COURSES\angular 6\application\code\ShoppingCartApp\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
